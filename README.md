8113e4edd09f2941fe58983d805aa90b9716156c
Razvan Predescu <razvan.predescu@dataireland.ie>
2019-02-23 21:48:02 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# Dynamic Component Load Example in Svelte
+
+This is a project template for [Svelte] showcasing dinamically component loading at runtime.
+
+The real thing here is that the sample component is not bundled at buildtime, but can be added at a post build/deployment time.
+
+The author of implementation is Rich Harris and his answer to the question about loading components dynamically at runtime in Svelte was posted on Stack Overflow:
+
+Is it possible to dynamically load a Svelte template at runtime?
+https://stackoverflow.com/a/50300657/778863
+
+## Technical Details
+
+The Chatbox is a simple Svelte component precompiled as ESM. Below you can see the required Rollup configuration:
+
+```javascript
+{
+	sourcemap: true,
+	format: 'esm',
+	name: 'Chatbox',
+	dir: 'public',
+	isDynamicEntry: true
+}
+```
+
+*Note that you will need to have [Node.js](https://nodejs.org) installed.*
+
+## Get started
+
+Clone the repository
+
+```bash
+git clone https://github.com/rzvdaniel/svelte-dynamic-import.git
+```
+
+Install the dependencies...
+
+```bash
+cd svelte-dynamic-import
+npm install
+```
+
+...then start [Rollup](https://rollupjs.org):
+
+```bash
+npm run dev
+```
+
+Navigate to [localhost:5000](http://localhost:5000).
+
+Clicking the Chat button will load the Chatbox component dynamically.
+
+
---
+++ public/chunk-b3db9592.js
@@ -0,0 +1,213 @@
+function noop() {}
+
+function assign(tar, src) {
+	for (var k in src) tar[k] = src[k];
+	return tar;
+}
+
+function callAfter(fn, i) {
+	if (i === 0) fn();
+	return () => {
+		if (!--i) fn();
+	};
+}
+
+function addLoc(element, file, line, column, char) {
+	element.__svelte_meta = {
+		loc: { file, line, column, char }
+	};
+}
+
+function run(fn) {
+	fn();
+}
+
+function append(target, node) {
+	target.appendChild(node);
+}
+
+function insert(target, node, anchor) {
+	target.insertBefore(node, anchor);
+}
+
+function detachNode(node) {
+	node.parentNode.removeChild(node);
+}
+
+function createElement(name) {
+	return document.createElement(name);
+}
+
+function createText(data) {
+	return document.createTextNode(data);
+}
+
+function createComment() {
+	return document.createComment('');
+}
+
+function addListener(node, event, handler, options) {
+	node.addEventListener(event, handler, options);
+}
+
+function removeListener(node, event, handler, options) {
+	node.removeEventListener(event, handler, options);
+}
+
+function setData(text, data) {
+	text.data = '' + data;
+}
+
+function blankObject() {
+	return Object.create(null);
+}
+
+function destroy(detach) {
+	this.destroy = noop;
+	this.fire('destroy');
+	this.set = noop;
+
+	this._fragment.d(detach !== false);
+	this._fragment = null;
+	this._state = {};
+}
+
+function destroyDev(detach) {
+	destroy.call(this, detach);
+	this.destroy = function() {
+		console.warn('Component was already destroyed');
+	};
+}
+
+function _differs(a, b) {
+	return a != a ? b == b : a !== b || ((a && typeof a === 'object') || typeof a === 'function');
+}
+
+function fire(eventName, data) {
+	var handlers =
+		eventName in this._handlers && this._handlers[eventName].slice();
+	if (!handlers) return;
+
+	for (var i = 0; i < handlers.length; i += 1) {
+		var handler = handlers[i];
+
+		if (!handler.__calling) {
+			try {
+				handler.__calling = true;
+				handler.call(this, data);
+			} finally {
+				handler.__calling = false;
+			}
+		}
+	}
+}
+
+function flush(component) {
+	component._lock = true;
+	callAll(component._beforecreate);
+	callAll(component._oncreate);
+	callAll(component._aftercreate);
+	component._lock = false;
+}
+
+function get() {
+	return this._state;
+}
+
+function init(component, options) {
+	component._handlers = blankObject();
+	component._slots = blankObject();
+	component._bind = options._bind;
+	component._staged = {};
+
+	component.options = options;
+	component.root = options.root || component;
+	component.store = options.store || component.root.store;
+
+	if (!options.root) {
+		component._beforecreate = [];
+		component._oncreate = [];
+		component._aftercreate = [];
+	}
+}
+
+function on(eventName, handler) {
+	var handlers = this._handlers[eventName] || (this._handlers[eventName] = []);
+	handlers.push(handler);
+
+	return {
+		cancel: function() {
+			var index = handlers.indexOf(handler);
+			if (~index) handlers.splice(index, 1);
+		}
+	};
+}
+
+function set(newState) {
+	this._set(assign({}, newState));
+	if (this.root._lock) return;
+	flush(this.root);
+}
+
+function _set(newState) {
+	var oldState = this._state,
+		changed = {},
+		dirty = false;
+
+	newState = assign(this._staged, newState);
+	this._staged = {};
+
+	for (var key in newState) {
+		if (this._differs(newState[key], oldState[key])) changed[key] = dirty = true;
+	}
+	if (!dirty) return;
+
+	this._state = assign(assign({}, oldState), newState);
+	this._recompute(changed, this._state);
+	if (this._bind) this._bind(changed, this._state);
+
+	if (this._fragment) {
+		this.fire("state", { changed: changed, current: this._state, previous: oldState });
+		this._fragment.p(changed, this._state);
+		this.fire("update", { changed: changed, current: this._state, previous: oldState });
+	}
+}
+
+function _stage(newState) {
+	assign(this._staged, newState);
+}
+
+function setDev(newState) {
+	if (typeof newState !== 'object') {
+		throw new Error(
+			this._debugName + '.set was called without an object of data key-values to update.'
+		);
+	}
+
+	this._checkReadOnly(newState);
+	set.call(this, newState);
+}
+
+function callAll(fns) {
+	while (fns && fns.length) fns.shift()();
+}
+
+function _mount(target, anchor) {
+	this._fragment[this._fragment.i ? 'i' : 'm'](target, anchor || null);
+}
+
+var protoDev = {
+	destroy: destroyDev,
+	get,
+	fire,
+	on,
+	set: setDev,
+	_recompute: noop,
+	_set,
+	_stage,
+	_mount,
+	_differs
+};
+
+export { assign as a, protoDev as b, init as c, createElement as d, addLoc as e, insert as f, noop as g, run as h, detachNode as i, createText as j, createComment as k, addListener as l, append as m, setData as n, callAfter as o, removeListener as p, flush as q };
+//# sourceMappingURL=chunk-b3db9592.js.map
